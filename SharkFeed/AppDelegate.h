1e96676f2008bc011fc88f8d8208b36f73cba5fd
Avigit Saha <saha.liton@gmail.com>
2016-02-16 21:53:36 -0800

    Initial Commit

---
+++ SharkFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SharkFeed
+//
+//  Created by Avigit Saha on 2/16/16.
+//  Copyright © 2016 Avigit Saha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
